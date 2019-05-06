dfa9fe33e381e7db4d9404d72c15507ca3894cb1
root <root@localhost.localdomain>
2019-05-06 12:20:00 -0400

    new file:   install_vim_go_plugin.sh

---
+++ install_vim_go_plugin.sh
@@ -0,0 +1,24 @@
+SRC_PATH=/opt/go/src/github.com
+
+#mkdir $SRC_PATH/fatih
+#cd $SRC_PATH/fatih
+#git clone https://github.com/fatih/motion
+#go install github.com/fatih/motion
+
+
+#mkdir $SRC_PATH/kisielk
+#cd $SRC_PATH/kisielk
+#git clone https://github.com/kisielk/errcheck
+#go install github.com/kisielk/errcheck
+
+mkdir $SRC_PATH/go-delve
+cd $SRC_PATH/go-delve
+git clone https://github.com/go-delve/delve/cmd/dlv
+go install github.com/go-delve/delve/cmd/dlv
+
+#github.com/mdempsky/gocode
+#github.com/josharian/impl
+#github.com/golangci/golangci-lint/cmd/golangci-lint
+#github.com/jstemmer/gotags
+#golang.org/x/tools/cmd/gorename
+
